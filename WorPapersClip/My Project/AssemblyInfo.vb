975941a5f54aea04cf33eb7c8b0e7b397cf3c48e
Worcome <worcome.studios@gmail.com>
2021-10-17 05:01:21 +0200

    main

---
+++ WorPapersClip/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Administrador de portapapeles")>
+<Assembly: AssemblyDescription("Administrador de portapapeles")>
+<Assembly: AssemblyCompany("Worcome Studios")>
+<Assembly: AssemblyProduct("PapersClip")>
+<Assembly: AssemblyCopyright("Copyright © 2021 Worcome Studios. Algunos derechos reservados.")>
+<Assembly: AssemblyTrademark("Worcome Studios")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("fc69bbc0-363c-4e6b-be2a-a78d83629d36")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("2.0.0.0")>
+<Assembly: AssemblyFileVersion("2.0.0.0")>
+<Assembly: NeutralResourcesLanguage("es")>
